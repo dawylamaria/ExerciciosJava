4d7b0b7ebf9ba4e8b937b08f92518f3015e97b00
dawylamaria <dawylamaria@gmail.com>
2023-02-17 10:43:18 -0300

    modificando questao 7

--- src/Questao7.java
+++ src/Questao7.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Questao7 {
+//Questao 7 da lista de exercicios
+import java.util.Scanner;
+public class Questao7 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int valor;
+
+        System.out.print("Digite o valor da tabuada que deseja calcular:");
+
+        valor = scan.nextInt();
+
+        for (int i = 1; i < 11; i++){
+            System.out.println(valor + " x "  + i + " = " + (valor * i));
+        }
+
+    }
 }
